34f3a1b6a08b046b8eaba13374f2c504708b22e8
Wen <985474749@qq.com>
2022-02-25 17:22:51 +0800

    tijiao

---
+++ src/main/java/com/springboot/demo/service/UserService.java
@@ -0,0 +1,16 @@
+package com.springboot.demo.service;
+
+import com.springboot.demo.eneity.User;
+import com.springboot.demo.mapper.UserMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+    @Autowired
+    UserMapper userMapper;
+    public User Sel(int id){
+        return userMapper.Sel(id);
+    }
+}
+
---
+++ src/main/java/com/springboot/demo/service/studentService.java
@@ -0,0 +1,4 @@
+package com.springboot.demo.service;
+
+public class studentService {
+}
